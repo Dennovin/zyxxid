30ae64939fdbfb03b279dea0abf7690231cac36e
Corey Cossentino <corey@cossentino.com>
2016-06-18 04:08:22 -0400

    some setup things

---
+++ app/setup.py
@@ -0,0 +1,26 @@
+import os
+from datetime import datetime
+from setuptools import setup, find_packages
+
+setup(
+    name="zyxxid",
+    description="zyxxid.xyz",
+    version="{0:%Y%m%d}.{0:%H%M%S}".format(datetime.utcnow()),
+    author="Corey Cossentino",
+    author_email="corey@cossentino.com",
+    packages=find_packages(),
+    package_data={
+    },
+    package_dir={
+        "zyxxid": "zyxxid",
+    },
+    install_requires=[
+        "Flask",
+        "Flask-Login",
+        "riak",
+        "PyYAML",
+        "uWSGI",
+        "celery",
+        ],
+#    scripts=["scripts/{}".format(i) for i in os.listdir("scripts")],
+    )
