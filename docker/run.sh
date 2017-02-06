d4db20c0dea488664e6a1c158e15b6502b8482d7
Corey Cossentino <corey@cossentino.com>
2017-02-06 14:01:22 -0500

    add a pidfile

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,4 @@
 /usr/sbin/service rabbitmq-server start
 /usr/sbin/service celeryd start
 /usr/sbin/service nginx start
-/data/venv/bin/uwsgi --ini /data/etc/uwsgi.ini --daemonize /data/logs/uwsgi_daemon.log
+/data/venv/bin/uwsgi --ini /data/etc/uwsgi.ini --pidfile /run/uwsgi.pid --daemonize /data/logs/uwsgi_daemon.log
